25a0e8a9fb4118ac167bdb10824c7be42f67615b
Bob Melvin <bob_melvin@comcast.net>
2015-07-02 08:16:59 -0400

    crudely disable SLO

--- app/controllers/devise/saml_sessions_controller.rb
+++ app/controllers/devise/saml_sessions_controller.rb
@@ -20,9 +20,9 @@ class Devise::SamlSessionsController < Devise::SessionsController
   protected
 
   # Override devise to send user to IdP logout for SLO
-  def after_sign_out_path_for(_)
-    request = OneLogin::RubySaml::Logoutrequest.new
-    request.create(@saml_config)
-  end
+  # def after_sign_out_path_for(_)
+  #   request = OneLogin::RubySaml::Logoutrequest.new
+  #   request.create(@saml_config)
+  # end
 end
 
